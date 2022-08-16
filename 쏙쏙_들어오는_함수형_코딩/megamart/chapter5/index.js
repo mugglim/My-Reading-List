cf1d5c73d792ef9e02c2484c071c9f43973b38f9
Jaeuk Lim <applewodnr@naver.com>
2022-08-16 22:15:44 +0900

    챕터5 megamart 미션 반영 및 구현

---
+++ 쏙쏙_들어오는_함수형_코딩/megamart/chapter5/index.js
@@ -0,0 +1,110 @@
+// data
+const BUY_BUTTONS = [
+	{ name: 'pen', price: 3 },
+	{ name: 'shoe', price: 5 },
+	{ name: 't-shirt', price: 10 },
+];
+
+// actions
+let shoppingCartTotal = 0;
+let tax = 0;
+let shoppingCart = [];
+
+function getBuyButtonsDOM() {
+	// Assume that data is a DOM node.
+	return BUY_BUTTONS;
+}
+
+function setCartTotalDOM(total) {
+	console.log(`total price : ${total}`);
+}
+
+function showFreeShippingIcon(name, price) {
+	console.log(`${name} is ${price}$, so it's free shipping!`);
+}
+
+function hideFreeShippingIcon(name, price) {
+	console.log(`${name} is ${price}$, so it's not free shipping!`);
+}
+
+function updateShippingIcons(cart) {
+	const $buyButtons = getBuyButtonsDOM();
+
+	for (const { name, price } of $buyButtons) {
+		// 1
+		const cartItem = makeCartItem(name, price);
+		const newCart = addItem(cart, cartItem);
+
+		// 2
+		const isShown = isFreeShippingItem(newCart);
+
+		// 3
+		setFreeShippingIcon(name, price, isShown);
+	}
+}
+
+function setFreeShippingIcon(name, price, isShown) {
+	if (isShown) {
+		showFreeShippingIcon(name, price);
+		return;
+	}
+
+	hideFreeShippingIcon(name, price);
+}
+
+function setTaxDOM(newTax) {
+	tax = newTax;
+	console.log(`tax is ${tax}`);
+}
+
+function updateTaxDOM(total) {
+	setTaxDOM(calcTax(total).toFixed(1));
+}
+
+function addItemToCart(name, price) {
+	const cartItem = makeCartItem(name, price);
+	shoppingCart = addItem(shoppingCart, cartItem);
+
+	const total = calcTotal(shoppingCart);
+	setCartTotalDOM(total);
+	updateShippingIcons(shoppingCart);
+	updateTaxDOM(total);
+}
+
+// calculations
+function appendElement(array, element) {
+	return array.slice().concat(element);
+}
+
+function addItem(cart, item) {
+	return appendElement(cart, item);
+}
+
+function makeCartItem(name, price) {
+	return { name, price };
+}
+
+function calcTotal(cart) {
+	return cart.reduce((total, { price }) => total + price, 0);
+}
+
+function isFreeShippingItem(cart) {
+	return calcTotal(cart) >= 20;
+}
+
+function calcTax(total) {
+	return total * 0.1;
+}
+
+// for testing
+console.log('---- 🛒 Add shoe ----');
+addItemToCart('shoe', 5);
+console.log();
+
+console.log('---- 🛒 Add t-shirt');
+addItemToCart('t-shirt', 10);
+console.log();
+
+console.log('---- 🛒 Add pen ----');
+addItemToCart('pen', 3);
+console.log();
